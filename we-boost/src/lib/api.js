2f8a0f09f2bc6b2166c2f630c6d50955537188a5
amutehjnr <mustaeenms@gmail.com>
2026-08-17 13:31:48 +0100

    Fix backend build errors

--- we-boost/src/lib/api.js
+++ we-boost/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://we-boost-64ej.vercel.app/api",
 });
 
 API.interceptors.request.use((config) => {
