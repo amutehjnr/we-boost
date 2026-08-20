9f27a4b7febd0a22aaee1b53e78a3e57a68827b0
amutehjnr <mustaeenms@gmail.com>
2026-08-20 13:35:58 +0100

    Remove unused profileLoaded state causing build failure 2

--- we-boost/src/components/user-components/user-dashboard/UserDahboardHome.jsx
+++ we-boost/src/components/user-components/user-dashboard/UserDahboardHome.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { FaWallet, FaCheckCircle, FaTasks, FaTrophy } from "react-icons/fa";
 import { useTheme } from "../../../context/ThemeContext"; // ✅ import your theme context
-import UserDashboardLayout from "./UserDashboardLayout";
 import API from "../../../lib/api";
 
 export default function UserDashboardHome() {
